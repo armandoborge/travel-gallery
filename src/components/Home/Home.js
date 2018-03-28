d5609eb91001d7a0dda7bcb7eae8b6dedac7b865
Armando Borge <aborge@elevensystems.com>
2018-03-28 20:02:05 -0300

    rel="noopener noreferrer" for links

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,11 +23,11 @@ const home = () => {
                 <h1>Mis Fotos de Viajes</h1>
                 <p>Software y viajes: así nace este proyecto, luego de algún tiempo viajando decidí poner en Internet mis fotografías favoritas de los lugares que he visitado.</p>
                 <p>No logré encontrar un diseño web de mi gusto, así que he creado una aplicación en Javascript usando ReactJS para construir lo que tenía en mi mente y a la vez divertirme un poco en el proceso.</p>
-                <p>El código fuente esta disponible en <a href="https://github.com/armandoborge/travel-gallery" target="_blank" title="Travel Gallery GitHub">GitHub</a>. En el camino iré mejorando el código y subiendo mas fotos.</p>
+                <p>El código fuente esta disponible en <a href="https://github.com/armandoborge/travel-gallery" target="_blank" rel="noopener noreferrer" title="Travel Gallery GitHub">GitHub</a>. En el camino iré mejorando el código y subiendo mas fotos.</p>
                 <p>Espero lo disfrutes!</p>
                 <ul className={styles.Links}>
                     <li>
-                        <a href="http://armandoborge.com/" target="_blank" title="Armando Borge">
+                        <a href="http://armandoborge.com/" target="_blank" title="Armando Borge" rel="noopener noreferrer">
                             <i className="fab fa-fort-awesome-alt" />
                             <span>Mi Website</span>
                         </a>
@@ -39,7 +39,7 @@ const home = () => {
                         </Link>
                     </li>
                     <li>
-                        <a href="https://github.com/armandoborge/travel-gallery" target="_blank" title="Travel Gallery GitHub">
+                        <a href="https://github.com/armandoborge/travel-gallery" target="_blank" title="Travel Gallery GitHub" rel="noopener noreferrer">
                             <i className="fab fa-github" />
                             <span>GitHub</span>
                         </a>
