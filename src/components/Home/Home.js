00742367db10c8ef6882ecebc92317fede0d628f
Armando Borge <aborge@elevensystems.com>
2018-04-02 21:16:49 -0300

    Home updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,7 +12,6 @@ import styles from './Home.css'
 import homeBackground from './home_bg.svg';
 
 const home = (props) => {
-    console.log(props.showSidebar);
     let homeBackgroundStyles = {
         background: 'url(' + homeBackground + ') center center / cover no-repeat'
     };
@@ -23,19 +22,19 @@ const home = (props) => {
         <div className={styles.Home} style={homeBackgroundStyles}>
             <div className={homeContentClasses.join(' ')}>
                 <h1>Fotos de Viaje</h1>
-                <p>Luego de algún tiempo viajando decidí poner en Internet mis fotos favoritas de los lugares que he visitado. No logré encontrar un diseño web de mi gusto, así que he creado una aplicación en Javascript usando ReactJS para construir lo que tenía en mi mente y divertirme un poco en el proceso.</p>
+                <p>Luego de algún tiempo viajando decidí poner en Internet mis <Link to='/gallery/argentina/el-calafate/'>fotos favoritas</Link> de los lugares que he visitado. No logré encontrar un diseño web de mi gusto, así que he creado una aplicación en Javascript usando <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer" title="React">React</a> para para construir lo que tenía en mi mente y divertirme un poco en el proceso.</p>
                 <p>El código fuente esta disponible en <a href="https://github.com/armandoborge/travel-gallery" target="_blank" rel="noopener noreferrer" title="Travel Gallery GitHub">GitHub</a>. En el camino iré haciendo mejoras y subiendo mas fotos.</p>
                 <ul className={styles.Links}>
                     <li>
                         <a href="http://armandoborge.com/" target="_blank" title="Armando Borge" rel="noopener noreferrer">
                             <i className="fas fa-chess" />
-                            <span>Mi Website</span>
+                            <span>Mi Web</span>
                         </a>
                     </li>
                     <li>
                         <Link to='/gallery/argentina/el-calafate/'>
                             <i className="fas fa-globe" />
-                            <span>Ir a Fotos</span>
+                            <span>Galería</span>
                         </Link>
                     </li>
                     <li>
